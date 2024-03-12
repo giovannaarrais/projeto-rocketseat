b36c63dc26bae62d13c855278c52fd75a0489ced
Giovanna Arrais <giovannaarrais13@gmail.com>
2024-03-12 11:34:27 -0300

    :tada: initial commi

---
+++ script.js
@@ -0,0 +1,26 @@
+/*ação de mudanca no claro e escuro*/
+
+function toggleMode() {
+  const html = document.documentElement
+   html.classList.toggle("light")
+
+
+/*pegar a tag img */
+  const img = document.querySelector("#profile img")
+
+
+/*susbtituir a imagem */
+  if(html.classList.contains('light')) {
+
+/*se tiver light mode, add a imagem light*/
+  img.setAttribute ( "src", "./assets/avatar-light.png")
+
+ } else {
+
+/*sse tiver sem light mode, manter a imagem normla */
+  img.setAttribute("src", "./assets/avatar.png" )
+
+ }
+}
+
+
